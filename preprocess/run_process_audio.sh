ef2a684d683d73d3780f1cb07e6519fdd11d5da7
ubppd@student.kit.edu <ubppd@uc2n995.localdomain>
2023-12-02 20:07:17 +0100

    job queued

--- preprocess/run_process_audio.sh
+++ preprocess/run_process_audio.sh
@@ -3,26 +3,44 @@
 # TODO must be completly on the GPU
 
 #SBATCH --job-name=PST_process_audio       # job name
-#SBATCH --partition=multiple_gpu           # TODO check gpu queue for the resource allocation.
-#SBATCH --time=30:00                       # TODO wall-clock time limit  
-#SBATCH --mem=40000                        # TODO memory per node
-#SBATCH --nodes=4                          # number of nodes to be used
-#SBATCH --cpus-per-task=40                 # number of CPUs required per MPI task
+#SBATCH --partition=gpu_4	           # TODO check gpu queue for the resource allocation.
+#SBATCH --time=1:00:00                       # TODO wall-clock time limit  
+#SBATCH --mem=200000                        # TODO memory per node
+#SBATCH --nodes=1                          # number of nodes to be used
+#SBATCH --cpus-per-task=1                 # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --mail-user=uxude@student.kit.edu  # notification email address
+#SBATCH --mail-user=ubppd@student.kit.edu  # notification email address
+#SBATCH --gres=gpu:4
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-export VENVDIR=~/env                       # Export path to your virtual environment.
+# export VENVDIR=~/env                       # Export path to your virtual environment.
 export PYDIR=.    			                   # Export path to directory containing Python script.
 
 module purge                                    # Unload all currently loaded modules.
 module load compiler/gnu/10.2                   # Load required modules.  
 module load devel/python/3.8.6_gnu_10.2
-module load mpi/openmpi/4.1
-module load devel/cuda/10.2
+# module load mpi/openmpi/4.1
+# module load devel/cuda/10.2
 
-source ${VENVDIR}/bin/activate  # Activate your virtual environment.
+# source ${VENVDIR}/bin/activate  # Activate your virtual environment.
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home//kit/stud/ubppd/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home//kit/stud/ubppd/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home//kit/stud/ubppd/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home//kit/stud/ubppd/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+	
+conda activate nmt
 
 # mpirun python ${PYDIR}/process_audio.py
 python ${PYDIR}/process_audio.py
