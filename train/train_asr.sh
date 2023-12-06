cf2317b156c83d8189bc0b199f53a12eef5102b9
Bertil Braun <bertil.braun.private@gmail.com>
2023-12-06 14:23:39 +0100

    Updated train_asr.sh to use conda

--- train/train_asr.sh
+++ train/train_asr.sh
@@ -11,21 +11,30 @@
 #SBATCH --mail-user=uxude@student.kit.edu  # notification email address
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-export VENVDIR=~/env                       # Export path to your virtual environment.
 export PYDIR=.    			                   # Export path to directory containing Python script.
 
 module purge                                    # Unload all currently loaded modules.
 module load compiler/gnu/10.2                   # Load required modules.  
 module load devel/python/3.8.6_gnu_10.2
-module load mpi/openmpi/4.1
-module load devel/cuda/10.2
+# module load mpi/openmpi/4.1
+# module load devel/cuda/10.2
 
-source ${VENVDIR}/bin/activate  # Activate your virtual environment.
-
-git clone https://github.com/facebookresearch/fairseq.git
-cd fairseq
-pip install --editable ./
-cd ..
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home//kit/stud/ubppd/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home//kit/stud/ubppd/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home//kit/stud/ubppd/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home//kit/stud/ubppd/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+	
+conda activate nmt
 
 export PYTHONPATH="~/fairseq/:$PYTHONPATH"
 # Define the model types in an array
