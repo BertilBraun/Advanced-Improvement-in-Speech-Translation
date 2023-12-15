adbdc903ba11a50f36e5d23db93bbe81f43c4d8f
Bertil Braun <bertil.braun.private@gmail.com>
2023-12-15 12:26:19 +0100

    Adjusted SBATCH for process audio

--- preprocess/run_process_audio.sh
+++ preprocess/run_process_audio.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=PST_process_audio       # job name
 #SBATCH --partition=gpu_4	               # check gpu queue for the resource allocation.
-#SBATCH --time=02:00:00                    # wall-clock time limit  
-#SBATCH --mem=200000                       # memory per node
+#SBATCH --time=01:00:00                    # wall-clock time limit  
+#SBATCH --mem=150000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
