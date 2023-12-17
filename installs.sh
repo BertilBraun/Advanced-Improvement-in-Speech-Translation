6274dc2054dd93c28e34045a170fe5c94ba29d43
Bertil Braun <bertil.braun.private@gmail.com>
2023-12-17 14:48:04 +0100

    Updated installs

--- installs.sh
+++ installs.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
-#SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
+#SBATCH --mail-type=NONE                   # Notify user by email when certain event types occur.
 #SBATCH --gres=gpu:1
 #SBATCH --output=log.txt
 #SBATCH --error=log.txt
