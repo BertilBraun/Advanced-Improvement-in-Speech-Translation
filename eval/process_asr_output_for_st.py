c4d61a26ea2f1d7ad334f609cba3b0a99947bf7d
Bertil Braun <bertil.braun.private@gmail.com>
2023-12-21 21:35:37 +0100

    Cleanup ST

--- eval/process_asr_output_for_st.py
+++ eval/process_asr_output_for_st.py
@@ -3,51 +3,53 @@ from deep_translator import GoogleTranslator
 import sentencepiece as spm
 
 
-
-def translate_file(input_file, output_file, src_lng, target_lng, spm_output_model_file):
-    print("Translating file...")
+parser = argparse.ArgumentParser()
+parser.add_argument("--ref_input_file", type=str)
+parser.add_argument("--ref_output_file", type=str)
+parser.add_argument("--hyp_input_file", type=str)
+parser.add_argument("--hyp_output_file", type=str)
+parser.add_argument("--spm_input_file", type=str)
+parser.add_argument("--spm_output_file", type=str)
+parser.add_argument("--src_lng", type=str, default="en")
+parser.add_argument("--target_lng", type=str, default="de")
+args = parser.parse_args()
     
-    with open(input_file, "r", encoding="utf-8") as f:
+
+def sample_print(data_list):
+    DELIMITER = "----------------------------------------"
+    print(DELIMITER)
+    for data in data_list[:2]:
+        print(data)
+    print(DELIMITER)
+
+
+def process_reference_file():
+    with open(args.ref_input_file, "r", encoding="utf-8") as f:
         lines = [line.strip() for line in f.readlines()]
         
-    translator = GoogleTranslator(source=src_lng, target=target_lng) 
-
-    print(f"Translating... (target language: {target_lng})")
-
-    translated = [
-        translator.translate(line)
+    lines = process_reference_text(lines)
+        
+    spm_output_model = spm.SentencePieceProcessor(model_file=args.spm_output_file)
+    
+    lines = [
+        " ".join(spm_output_model.encode(line.strip(), out_type=str))
         for line in lines
     ]
     
-    print("Translated file")
-    for line in translated[:2]:
-        print(line)
-        
-        
-    spm_output_model = spm.SentencePieceProcessor(model_file=spm_output_model_file)
-    
-    translated = [
-        " ".join(spm_output_model.encode(line.strip(), out_type=str))
-        for line in translated
-    ]
-    
     print("Encoded output file")
-    for line in translated[:2]:
-        print(line)
+    sample_print(lines)
 
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(translated))
-        
-    print("Done translating file!")
+    with open(args.ref_output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(lines))
 
 
-def process_hypothesis_file(input_file, output_file, spm_input_model_file, spm_output_model_file):
+def process_hypothesis_file():
     print("Processing hypothesis file...")
-    with open(input_file, "r", encoding="utf-8") as f:
+    with open(args.hyp_input_file, "r", encoding="utf-8") as f:
         lines = [line.strip() for line in f.readlines()]
         
-    spm_input_model = spm.SentencePieceProcessor(model_file=spm_input_model_file)
-    spm_output_model = spm.SentencePieceProcessor(model_file=spm_output_model_file)
+    spm_input_model = spm.SentencePieceProcessor(model_file=args.spm_input_file)
+    spm_output_model = spm.SentencePieceProcessor(model_file=args.spm_output_file)
     
     lines = [
         spm_input_model.decode(line.split())
@@ -55,8 +57,9 @@ def process_hypothesis_file(input_file, output_file, spm_input_model_file, spm_o
     ]
     
     print("Decoded input file")
-    for line in lines[:2]:
-        print(line)
+    sample_print(lines)
+    
+    lines = process_hypothesis_text(lines)
         
     lines = [
         " ".join(spm_output_model.encode(line.strip(), out_type=str))
@@ -64,35 +67,44 @@ def process_hypothesis_file(input_file, output_file, spm_input_model_file, spm_o
     ]
     
     print("Encoded output file")
-    for line in lines[:2]:
-        print(line)
+    sample_print(lines)
             
-    with open(output_file, "w", encoding="utf-8") as f:
+    with open(args.hyp_output_file, "w", encoding="utf-8") as f:
         for line in lines:
             f.write(line + "\n")
             
     print("Done processing hypothesis file!")
 
-if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ref_input_file", type=str)
-    parser.add_argument("--ref_output_file", type=str)
-    parser.add_argument("--hyp_input_file", type=str)
-    parser.add_argument("--hyp_output_file", type=str)
-    parser.add_argument("--spm_input_file", type=str)
-    parser.add_argument("--spm_output_file", type=str)
-    parser.add_argument("--src_lng", type=str, default="en")
-    parser.add_argument("--target_lng", type=str, default="de")
-    args = parser.parse_args()
+def process_reference_text(lines):
+    # TODO this should be temporary as we should get a ST dataset in the future
+    # TODO otherwise, this should at least be the original reference text without any processing, which we do during ASR preprocessing
+    print("Translating file...")
     
-    # Use the Google Translate API to translate the input file
-    # and write the output to the output file.
+    translator = GoogleTranslator(source=args.src_lng, target=args.target_lng) 
+
+    print(f"Translating... (target language: {args.target_lng})")
+
+    translated = translator.translate_batch(lines)
     
+    print("Translated file")
+    sample_print(translated)
+    
+    return translated
+
+
+def process_hypothesis_text(lines):
+    # TODO process hypothesis text, for example, add punctuation, capitalization, etc.
+    # TODO use a LLM to post-process the hypothesis text
+    return lines
+        
+
+
+if __name__ == "__main__":
     print("Starting processing...")
     
-    translate_file(args.ref_input_file, args.ref_output_file, args.src_lng, args.target_lng, args.spm_output_file)
+    process_reference_file()
     
-    process_hypothesis_file(args.hyp_input_file, args.hyp_output_file, args.spm_input_file, args.spm_output_file)
+    process_hypothesis_file()
     
     print("Done processing!")
