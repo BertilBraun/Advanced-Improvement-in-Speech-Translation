92cb64ae51996648239ecd0c0cc0287490795a6a
Bertil Braun <bertil.braun.private@gmail.com>
2024-01-13 19:13:12 +0100

    Changed train parameters

--- train/train_mt.sh
+++ train/train_mt.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=train_mt                # job name
 #SBATCH --partition=gpu_4                  # mby GPU queue for the resource allocation.
-#SBATCH --time=12:00:00                    # wall-clock time limit
+#SBATCH --time=14:00:00                    # wall-clock time limit
 #SBATCH --mem=200000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
@@ -63,7 +63,7 @@ fairseq-train \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --keep-last-epochs 1 --save-interval-updates 50000 --keep-best-checkpoints 1 \
     --max-tokens 2048 \
-    --max-epoch 500 \
+    --max-epoch 150 \
     --fp16
 
 echo "Training complete."
