b66459505a0c21c802bc9c1a19ffc0966240e07f
Bertil Braun <bertil.braun.private@gmail.com>
2024-01-14 17:22:35 +0100

    Reduced train time

--- train/train_mt.sh
+++ train/train_mt.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=train_mt                # job name
 #SBATCH --partition=gpu_4                  # mby GPU queue for the resource allocation.
-#SBATCH --time=14:00:00                    # wall-clock time limit
+#SBATCH --time=06:00:00                    # wall-clock time limit
 #SBATCH --mem=200000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
