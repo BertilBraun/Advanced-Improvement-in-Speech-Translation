05404a40925214a680657df7f82426bcc46b76d1
Vandool <arvandkaveh@gmail.com>
2024-01-14 21:29:31 +0100

    covost split script modified

--- preprocess/run_get_covost_split.sh
+++ preprocess/run_get_covost_split.sh
@@ -4,7 +4,7 @@ source ../setup.sh
 
 ASR="/pfs/work7/workspace/scratch/ubppd-ASR/"
 COVOST="$ASR/covost"
-TRANSLATIONS="$COVOST/data/translations"
+ROOT="$COVOST/data/translations"
 VALIDATED="$COVOST/corpus-16.1/en/validated.tsv"
 
 cd "$COVOST" || exit
@@ -12,5 +12,14 @@ cd "$COVOST" || exit
 # Run the Python script with direct path constructions
 python get_covost_splits.py \
   --version 2 --src-lang en --tgt-lang de \
-  --root "$TRANSLATIONS" \
+  --root "$ROOT" \
   --cv-tsv "$VALIDATED"
+
+
+# This will result in
+# You should get 3 TSV files (covost.<src_lang_code>_<tgt_lang_code>.<split>.tsv) for train, dev and test splits,
+# respectively in --root Directory.
+#   Each of them has 4 columns: 
+#   path (audio filename), 
+#   sentence (transcript), 
+#   translation and client_id (speaker ID).
