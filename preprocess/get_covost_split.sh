1b70ddbb4d4938eb18e6e87915977a282aca87c3
Vandool <arvandkaveh@gmail.com>
2024-01-14 21:17:51 +0100

    get_covost_splits modified

--- preprocess/get_covost_split.sh
+++ preprocess/get_covost_split.sh
@@ -3,12 +3,10 @@
 source ../setup.sh
 
 COVOST="/pfs/work7/workspace/scratch/ubppd-ASR/covost"
-
-cd "$WD" || exit
-
-# More descriptive directory names
 TRANSLATIONS="/pfs/work7/workspace/scratch/ubppd-ASR/covost/data/tt"
 VALIDATED="/pfs/work7/workspace/scratch/ubppd-ASR/covost/corpus-16.1/en"
+
+cd "$COVOST" || exit
 
 # Run the Python script with direct path constructions
 python get_covost_splits.py \
