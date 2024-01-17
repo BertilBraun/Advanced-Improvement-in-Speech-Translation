8da0de190e25329c7d9778a0168a66537cd74d35
Vandool <arvandkaveh@gmail.com>
2024-01-17 21:52:44 +0100

    working on covost

--- eval/run_covost_for_eval.sh
+++ eval/run_covost_for_eval.sh
@@ -8,6 +8,8 @@ echo "Setup script sourced successfully."
 # Set log level
 export LOGLEVEL="INFO"  # ["INFO", "DEBUG", ...]
 
+cd "$HOME/Advanced-Improvement-in-Speech-Translation" || { echo "Could not change directory. Exiting script ..."; exit 1;}
+
 #python -m preprocess.process_audio_covost_wav2vec
 python -m eval.prepare_covost_for_eval
 
