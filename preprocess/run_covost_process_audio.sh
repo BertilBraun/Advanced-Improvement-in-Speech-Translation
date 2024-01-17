cabd35f77b834b662bdb133af3745503b1c0203b
Vandool <arvandkaveh@gmail.com>
2024-01-17 19:45:34 +0100

    working on covost

--- preprocess/run_covost_process_audio.sh
+++ preprocess/run_covost_process_audio.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#SBATCH --job-name=covost_preprocess     # job name
+#SBATCH --partition=dev_gpu_4                  # mby GPU queue for the resource allocation.
+#SBATCH --time=00:30:00                    # wall-clock time limit  
+#SBATCH --mem=100000                       # memory per node
+#SBATCH --nodes=1                          # number of nodes to be used
+#SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
+#SBATCH --ntasks-per-node=1                # maximum count of tasks per node
+#SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
+#SBATCH --gres=gpu:1
+
 echo "Starting the script execution."
 
 source ../setup.sh
