4253a417bd571eba98e2bc4f3b048ff66f33cc6a
Vandool <arvandkaveh@gmail.com>
2024-01-17 14:59:26 +0100

    working on covost

--- preprocess/process_audio_covost.py
+++ preprocess/process_audio_covost.py
@@ -1,20 +1,45 @@
 import csv
 import os
+from collections import defaultdict
 
-# Access the COVOST_ROOT environment variable
-covost_root = os.environ.get('COVOST_ROOT')
-if not covost_root:
-    raise EnvironmentError("COVOST_ROOT environment variable is not set")
 
-# Construct the file path
-file_path = os.path.join(covost_root, 'covost_v2.en_de.test.tsv')
+def read_data_table():
+    # Access the COVOST_ROOT environment variable
+    covost_root = os.environ.get("COVOST_ROOT")
+    if not covost_root:
+        raise EnvironmentError("COVOST_ROOT environment variable is not set")
 
-# Read and parse the TSV file
-data = []
-with open(file_path, 'r', encoding='utf-8') as file:
-    reader = csv.DictReader(file, delimiter='\t', fieldnames=['audio_path', 'english_sentence', 'german_translation', 'client_id'])
-    for row in reader:
-        data.append(row)
+    # Construct the file path
+    file_paths = {
+        "train": os.path.join(covost_root, "covost_v2.en_de.train.tsv"),
+        "test": os.path.join(covost_root, "covost_v2.en_de.test.tsv"),
+        "dev": os.path.join(covost_root, "covost_v2.en_de.dev.tsv"),
+    }
 
-# Now 'data' contains your data as a list of dictionaries
-print(data)
+    data = defaultdict(list)
+
+    # Read and parse each TSV file
+    for split, file_path in file_paths.items():
+        with open(file_path, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(
+                file,
+                delimiter="\t",
+                fieldnames=[
+                    "audio_path",
+                    "english_sentence",
+                    "german_translation",
+                    "client_id",
+                ],
+            )
+            for row in reader:
+                data[split].append(row)
+
+    return data
+
+
+if __name__ == "__main__":
+    counter = 3
+    for split, d in read_data_table().items():
+        print(split)
+        for i in range(counter):
+            print(d[i])
