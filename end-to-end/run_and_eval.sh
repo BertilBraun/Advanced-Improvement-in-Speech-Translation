50caf3ae3e9e846cbd3290e4b0efebbc7bc023aa
MaBo00 <bohnert.marius@web.de>
2024-01-19 00:21:23 +0100

    working on e2e

--- end-to-end/run_and_eval.sh
+++ end-to-end/run_and_eval.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=process_end_to_end      # job name
-#SBATCH --partition=gpu_4	               # check gpu queue for the resource allocation.
-#SBATCH --time=24:00:00                    # wall-clock time limit  
+#SBATCH --partition=dev_gpu_4	               # check gpu queue for the resource allocation.
+#SBATCH --time=00:30:00                    # wall-clock time limit  
 #SBATCH --mem=200000                        # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --output=process_end_to_end_logs_%j.txt
 #SBATCH --error=process_end_to_end_logs_%j.txt
 
