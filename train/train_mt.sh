3cbe59f9e5a3de02de9f4173ca78d472dc67ed3e
Bertil Braun <bertil.braun.private@gmail.com>
2024-01-18 09:01:54 +0100

    Updated train params

--- train/train_mt.sh
+++ train/train_mt.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name=train_mt                # job name
 #SBATCH --partition=gpu_4                  # mby GPU queue for the resource allocation.
-#SBATCH --time=16:00:00                    # wall-clock time limit
+#SBATCH --time=06:00:00                    # wall-clock time limit
 #SBATCH --mem=200000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --output=train_mt_logs_%j.txt
 #SBATCH --error=train_mt_logs_%j.txt
 
