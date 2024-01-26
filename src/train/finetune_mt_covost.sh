ba9c8fa42778b3082884cb42cf59fe96be95c471
Bertil Braun <bertil.braun.private@gmail.com>
2024-01-26 18:57:57 +0100

    Fixed path

--- src/train/finetune_mt_covost.sh
+++ src/train/finetune_mt_covost.sh
@@ -20,7 +20,7 @@ MT_WORKSPACE=/pfs/work7/workspace/scratch/uxude-MT
 TRAIN_WORKSPACE=$MT_WORKSPACE/train/finetune_mt_covost
 
 DATA_DIR=$MT_WORKSPACE/dataset/covost/spm
-BINARIZED_DATA_DIR=$TRAIN_WORKSPACE/binarized_dataset
+BINARY_DATA_DIR=$TRAIN_WORKSPACE/binarized_dataset
 MODEL_DIR=$TRAIN_WORKSPACE/models
 
 TRAIN_TIME_IN_HOURS=6
