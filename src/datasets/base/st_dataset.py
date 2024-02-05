a2d7f30a797f2569c19c90960785e979cbc16a77
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-05 10:35:10 +0100

    updated st dataset filtering

--- src/datasets/base/st_dataset.py
+++ src/datasets/base/st_dataset.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from pathlib import Path
 from typing import Optional
 
@@ -33,10 +34,10 @@ class STDataset(Dataset[DataSample]):
     def __filter_data(self) -> list[DataSample]:
         filtered_data: list[DataSample] = []
         
-        for path, sentence, translation, speaker_id, sample_id in self._data:
-            if not path.is_file():
-                logger.warning(f"Missing audio file for {speaker_id}-{sample_id}!")
-                continue
+        for path, sentence, translation, speaker_id, sample_id in tqdm(self._data, desc=f"Filtering {self.split} data"):
+            # if not path.is_file():
+            #     logger.warning(f"Missing audio file for {speaker_id}-{sample_id}!")
+            #     continue
             
             # if sentence is not a string, skip this sample
             if not isinstance(sentence, str) or \
