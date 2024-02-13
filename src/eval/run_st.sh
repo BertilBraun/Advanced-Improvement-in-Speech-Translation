90abec421f8447295123f0af1fc5b4b1327fb4a0
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-13 20:58:16 +0100

    Update run_st.sh

--- src/eval/run_st.sh
+++ src/eval/run_st.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=eval_st                 # job name
-#SBATCH --partition=dev_gpu_4                  # mby GPU queue for the resource allocation.
-#SBATCH --time=00:30:00                    # wall-clock time limit  
+#SBATCH --partition=gpu_4                  # mby GPU queue for the resource allocation.
+#SBATCH --time=02:00:00                    # wall-clock time limit  
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
